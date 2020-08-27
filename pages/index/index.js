89886377d6bf6a48829eb7897f750a5df3cb1074
pengbj1 <pengbj1@yonyou.com>
2020-08-28 00:45:59 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,51 @@
+Page({
+  data: {
+    code: "",
+    start_date:"",
+    end_date:"",
+    codeUrl:""
+  },
+
+  bindStartChange: function(e) {
+    this.setData({
+      start_date:e.detail.value
+    })
+  },
+
+  bindEndChange: function(e) {
+    this.setData({
+      end_date:e.detail.value
+    })
+  },
+
+  bindKeyInput: function(e) {
+    this.setData({
+      code:e.detail.value
+    })
+  },
+
+  search:function() {
+    let that = this
+    wx.request({
+      url: 'http://127.0.0.1:8000/search/?code='+this.data.code+'&start_date='+this.data.start_date+'&end_date='+this.data.end_date,
+      method:"GET",
+      responseType:"arraybuffer",
+      success:function(resp) {
+        console.log(resp)
+        let url = 'data:image/png;base64,' +wx.arrayBufferToBase64(resp.data)
+        that.setData({
+          codeUrl :url,
+          show :true
+        })
+      }
+    })
+  },
+
+  clickImg:function(event){
+    let that = this
+    wx.previewImage({
+      current:that.data.codeUrl,
+      urls:[that.data.codeUrl]
+    })
+  }
+})
